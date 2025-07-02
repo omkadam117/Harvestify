0f4e2c5708c326e1f75edc2928d45552b4407c1a
om kadam <165410063+omkadam117@users.noreply.github.com>
2025-07-02 11:43:11 +0530

    Delete utils directory

--- utils/fertilizer.py
+++
@@ -1,8 +0,0 @@
-fertilizer_dic = {
-    'NHigh': 'Your soil has high nitrogen content. Avoid nitrogen-based fertilizers for now.',
-    'Nlow': 'Your soil has low nitrogen content. Consider using urea or compost to increase nitrogen.',
-    'PHigh': 'Your soil has high phosphorous content. Avoid phosphate-based fertilizers.',
-    'Plow': 'Your soil has low phosphorous. Use fertilizers like SSP or bone meal.',
-    'KHigh': 'Your soil has high potassium content. Avoid potash-rich fertilizers.',
-    'Klow': 'Your soil has low potassium. Use MOP (Muriate of Potash) or wood ash.',
-}
